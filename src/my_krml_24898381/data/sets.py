f3be39efbac3bfea679a0cc1a733d8591a2478fb
santiago <santiagomm03@gmail.com>
2024-09-09 21:51:32 +1000

    add classifier

--- src/my_krml_24898381/data/sets.py
+++ src/my_krml_24898381/data/sets.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import os.path
 
 def pop_target(df, target_col):
     """Extract target variable from dataframe
@@ -47,8 +50,6 @@ def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y
     Returns
     -------
     """
-    import numpy as np
-
     if X_train is not None:
       np.save(f'{path}X_train', X_train)
     if X_val is not None:
@@ -86,8 +87,7 @@ def load_sets(path='../data/processed/'):
     Numpy Array
         Target for the testing set
     """
-    import numpy as np
-    import os.path
+   
 
     X_train = np.load(f'{path}X_train.npy', allow_pickle=True) if os.path.isfile(f'{path}X_train.npy') else None
     X_val   = np.load(f'{path}X_val.npy'  , allow_pickle=True) if os.path.isfile(f'{path}X_val.npy')   else None
@@ -157,5 +157,41 @@ def split_sets_by_time(df, target_col, test_ratio=0.2):
     X_train, y_train = subset_x_y(target=target, features=df_copy, start_index=0, end_index=-cutoff*2)
     X_val, y_val     = subset_x_y(target=target, features=df_copy, start_index=-cutoff*2, end_index=-cutoff)
     X_test, y_test   = subset_x_y(target=target, features=df_copy, start_index=-cutoff, end_index=len(df_copy))
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def split_sets_random(features, target, test_ratio=0.2):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    features : pd.DataFrame
+        Input dataframe
+    target : pd.Series
+        Target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    
+
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
 
     return X_train, y_train, X_val, y_val, X_test, y_test
